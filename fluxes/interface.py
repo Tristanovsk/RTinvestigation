5d4c3dbe49e3ca748789fc781e7f12213677d40f
Harmel Tristan <tristan.harmel@get.omp.eu>
2018-12-11 14:49:36 +0100

    add plotly

--- fluxes/interface.py
+++ fluxes/interface.py
@@ -1,6 +1,8 @@
 import sys, os
 import numpy as np
+import plotly.offline as po
 import pandas as pd
+
 import datetime
 import glob
 
@@ -9,9 +11,9 @@ import glob
 '''
 
 cmap = 'viridis'
-files = '/DATA/VRTC/OSOAA_profile/RESULTS/PROFILE/fluxes*'
+files = '/DATA/VRTC/OSOAA_profile/RESULTS/PROFILE/fluxes*chl0.03_sed0.00'
 files = glob.glob(files)
-
+files.sort()
 
 def info2dict(info):
     '''convert information content from first line (command line) of the LUT files
@@ -51,7 +53,8 @@ for file in files:
     df = df.append(df_)
 
 df[(df.Z < 100000) & (df.Z >= 0)].plot.scatter('Ed', 'Z', c='SZA', colormap=cmap, title=name)
-df[(df.Z < 0) & (df.Z > -50)].plot.scatter('Ed', 'Z', c='SZA', colormap=cmap, title=name)
+df[(df.Z < 0) & (df.Z > -10)].plot.scatter('Eo', 'Z', c='c', colormap=cmap, title=name,logx=True)
+
 df[(df.Z < 0.01) & (df.Z > -0.0015)].plot.scatter('Eu', 'Z', c='SZA', colormap=cmap, title=name)
 
 df_p = df[df.Z == 0]
@@ -60,7 +63,30 @@ df_m = df[df.Z == -0.001]
 df_p = df[df.LEVEL == 76]
 df_m = df[df.LEVEL == 77]
 
-df_ =  df[df.LEVEL == 80]
+df_ = df[df.LEVEL == 80]
 df_['eps'] = np.array(df_p.Ed - df_p.Eu + np.array(-df_m.Ed + df_m.Eu))
 df_['wl'] = np.array(df_m['OSOAA.Wa'], dtype=np.float) * 1000
-df_[['eps','SZA','wl','c']].plot.scatter(x='SZA',y='eps',c='wl',colormap=cmap)
+df_['c_over_a']= df_.c/(df_.aw+df_.achl+df_.amin+ df_.acdom)
+df_[['eps', 'SZA', 'wl', 'c']].plot.scatter(x='SZA', y='eps', c='c', colormap=cmap)
+#df_=df_[df_.achl < 10]
+fig = {
+    'data': [
+  		{
+        'x': df_.SZA,
+        'y': df_.eps,
+        'text':df_.wl,
+        'mode': 'markers',
+        'marker':dict(
+        size=16,
+        color = df_.c_over_a, #set color equal to a variable
+        colorscale='Viridis',
+        showscale=True)},
+
+    ],
+    'layout': {
+        'xaxis': {'title': 'SZA'},
+        'yaxis': {'title': 'eps'}
+    }
+}
+
+po.plot(fig)
